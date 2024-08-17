85dd7c1d786197d4ad768eb135a7b48a448c039b
Krishna Paul <89384077+KrishnapaulGit@users.noreply.github.com>
2024-08-17 20:44:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/jiocinema-clone">
+    <BrowserRouter basename="/KrishnapaulGit/jiocinema-clone">
       <App />
     </BrowserRouter>
   </React.StrictMode>
